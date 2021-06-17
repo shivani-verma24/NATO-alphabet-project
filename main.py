f55bad4c075c5e5fdfcb6a8bed889b5598d2bbe6
shivani-verma24 <86061698+shivani-verma24@users.noreply.github.com>
2021-06-17 21:56:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+alphabet_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+
+
+word = input("Enter a word: ").upper()
+
+word_code = [alphabet_dict[letter] for letter in word]
+
+print(word_code)
+
+# Project by Shivani Verma
