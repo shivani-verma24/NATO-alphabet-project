7517a591619a0de2514d4af3d8253f0f9d4d041a
Shivani verma <86061698+shivani-verma24@users.noreply.github.com>
2021-06-20 15:09:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-alphabet_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+alphabet_dict = {row.letter : row.code for (index, row) in data.iterrows()}
 
 
-word = input("Enter a word: ").upper()
+def generate_phonetic():
 
-word_code = [alphabet_dict[letter] for letter in word]
+    word = input("Enter a word: ").upper()
+    try:
+        word_code = [alphabet_dict[letter] for letter in word]
 
-print(word_code)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+
+    else:
+        print(word_code)
+
+generate_phonetic()
+
 
 # Project by Shivani Verma
